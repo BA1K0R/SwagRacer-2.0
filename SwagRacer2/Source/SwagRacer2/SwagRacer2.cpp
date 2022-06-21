30b59fc1b46e521cdfb28a2da03bcd239236e260
cowhahn <88606302+cowhahn@users.noreply.github.com>
2022-06-21 13:40:24 -0400

    c++

---
+++ SwagRacer2/Source/SwagRacer2/SwagRacer2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SwagRacer2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SwagRacer2, "SwagRacer2" );
---
+++ SwagRacer2/Source/SwagRacer2/SwagRacer2GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwagRacer2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWAGRACER2_API ASwagRacer2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
